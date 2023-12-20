d91b93170528b62ee154d0df3242ab4c6b21eefd
hailey201 <122054101+hailey201@users.noreply.github.com>
2023-12-20 12:28:36 -0800

    Player, Enemy, Audio

--- Player.gd
+++ Player.gd
@@ -1,11 +1,16 @@
 extends Node2D
 
+@export var speed = 15
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _process(_delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if velocity.length() >0:
+		velocity = velocity.normalized() * speed
